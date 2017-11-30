b3f1a13a7276bbd6a4de9762d378f40e25795872
Dayne May <dmay@beyondtrust.com>
2017-11-30 10:48:28 -0400

    Add all realtime data URLs to test.py

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 import requests
-import pygtfs
-
-GTFS_URL = "http://gtfs.halifax.ca/realtime/Vehicle/VehiclePositions.pb"
-
-
 from google.transit import gtfs_realtime_pb2
 
+URLS = {
+  'vehicle_positions': 'http://gtfs.halifax.ca/realtime/Vehicle/VehiclePositions.pb',
+  'alerts': 'http://gtfs.halifax.ca/realtime/Alert/Alerts.pb',
+  'trip_updates': 'http://gtfs.halifax.ca/realtime/TripUpdate/TripUpdates.pb'
+}
+
+
+response = requests.get(URLS['vehicle_positions'])
+
 feed = gtfs_realtime_pb2.FeedMessage()
-response = requests.get(GTFS_URL)
 feed.ParseFromString(response.content)
+
 for entity in feed.entity:
   print (entity)
-  if entity.HasField('trip_update'):
-    print ( entity.trip_update)
 
 
 
